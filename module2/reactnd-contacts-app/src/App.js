af85dc9a1b77575d82036a6bddda2e74cdaeb09b
higorcoliveira <higorcoliveira@gmail.com>
2018-12-20 21:06:10 -0200

    Componentes Controlados, propTypes, setState

--- module2/reactnd-contacts-app/src/App.js
+++ module2/reactnd-contacts-app/src/App.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
 class App extends Component {
+  // definindo o estado inicial da aplicação
+  // essa sintaxe só é possível graças ao processo de transpiler do babel
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
+  // o estado sempre é alterado no componente que detem o dado
+  // é possível alterar o estado de outra forma também, fazendo merge do objeto
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts 
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}  // é possível passar uma função como props
+        />
       </div>
     );
   }
